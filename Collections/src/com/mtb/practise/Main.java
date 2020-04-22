e7ec54d65b882d64a5423c4e627d6709f506ec6c
Mithilesh Bhusari <Mithilesh.Bhusari@sigma-systems.com>
2020-04-22 10:31:12 +0530

    Collections example

---
+++ Collections/src/com/mtb/practise/Main.java
@@ -0,0 +1,21 @@
+package com.mtb.practise;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Theatre theatre=new Theatre("Sudama",8,12);
+        theatre.getSeats();
+
+        if(theatre.reserveSeat("A10")){
+            System.out.println("Please pay");
+        }else {
+            System.out.println("Sorry, seat is taken");
+        }
+
+        if(theatre.reserveSeat("A10")){
+            System.out.println("Please pay");
+        }else {
+            System.out.println("Sorry, seat is taken");
+        }
+    }
+}
