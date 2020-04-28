be5a1257f2214f807330ad2480b2ceef48f6e461
Mithilesh Bhusari <MithileshB@sigmasys.net>
2020-04-28 09:30:52 +0530

    Created TopicController and Topic Pojo

---
+++ src/main/java/com/mtb/springbootpractise/topics/TopicController.java
@@ -0,0 +1,21 @@
+package com.mtb.springbootpractise.topics;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics(){
+        return Arrays.asList(
+                new Topic("Spring","Spring Framework","Spring Framework Description"),
+                new Topic("Java","Core Java","Core Java Description"),
+                new Topic("Javascript","Javascript","Javascript Description")
+        );
+    }
+}
