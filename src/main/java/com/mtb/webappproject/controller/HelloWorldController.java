426fd5dc19ff6514f53698ccd4ebeb0004667385
Mithilesh Bhusari <MithileshB@sigmasys.net>
2020-05-02 21:09:44 +0530

    Removing javascript and css file

--- src/main/java/com/mtb/webappproject/controller/HelloWorldController.java
+++ src/main/java/com/mtb/webappproject/controller/HelloWorldController.java
@@ -1,48 +1,27 @@
 package com.mtb.webappproject.controller;
 
 import com.mtb.webappproject.model.User;
-import com.mtb.webappproject.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloWorldController {
 
-        @RequestMapping(value="/")
-        public String mainPage(){
-            System.out.println("This is the main Page....Redirecting to  signup page");
-         return "signup.html";
-        }
-
+    @RequestMapping(value = "/")
+    public String mainPage() {
+        System.out.println("This is the main Page");
+        return "register.html";
+    }
 
 
     @RequestMapping(value = "/registerUser",
             method = RequestMethod.POST)
-    public   String registerUser(final  User user,Model model) {
+    public String registerUser(final User user, Model model) {
         System.out.println("Email: " + user.getEmail());
-        //: " + user.getEmail());
-   /*     System.out.println("httpServletRequest: " + httpServletRequest);
-        System.out.println("httpServletResponse: " + httpServletResponse);*/
-/*   RedirectView redirectView=new RedirectView("welcome");
-   redirectView.addStaticAttribute("email",user.getEmail());*/
-    model.addAttribute("email",user.getEmail());
+        model.addAttribute("email", user.getEmail());
         return "welcome";
     }
-
-/*    @RequestMapping(value="/welcome"  )
-    public String  welcome(Model model){
-         *//*   ModelAndView modelAndView = new ModelAndView();
-            modelAndView.setViewName("signup");
-            return modelAndView;*//*
-         //model.addAttribute("email",)
-         return "welcome.html";
-    }*/
 
 }
