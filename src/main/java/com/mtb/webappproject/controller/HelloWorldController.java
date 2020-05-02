8d15cc88ec729b440dd96639ed842aa224144480
Mithilesh Bhusari <MithileshB@sigmasys.net>
2020-05-02 13:18:49 +0530

    Adding Signup page in resources

--- src/main/java/com/mtb/webappproject/controller/HelloWorldController.java
+++ src/main/java/com/mtb/webappproject/controller/HelloWorldController.java
@@ -3,12 +3,16 @@ package com.mtb.webappproject.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class HelloWorldController {
 
-        @RequestMapping("/hello")
-        public String helloWorld(){
-            return "Hello World";
+        @RequestMapping("/")
+        public String  signup(){
+         /*   ModelAndView modelAndView = new ModelAndView();
+            modelAndView.setViewName("signup");
+            return modelAndView;*/
+         return "signup.html";
         }
 }
