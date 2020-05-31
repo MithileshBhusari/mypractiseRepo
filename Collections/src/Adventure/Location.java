9f71bc71f151fdd092c9c09c509964f69dd6ad1e
MithileshB <mitb@tdc.dk>
2020-05-31 20:58:13 +0530

    Adventure Game adding exits and location details

--- Collections/src/Adventure/Location.java
+++ Collections/src/Adventure/Location.java
@@ -13,6 +13,7 @@ public class Location {
         this.locationId=locationId;
         this.description=description;
         this.exits=new HashMap<String,Integer>();
+        exits.put("Q",0);
     }
 
     public void addExit(String direction,Integer locationId){
