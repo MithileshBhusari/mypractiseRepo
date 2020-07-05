02b94c64f7983529be92dc5809b22f479cb8c4aa
MithileshB <mitb@tdc.dk>
2020-07-05 07:51:48 +0530

    Different ways of Initializing Set

--- spring-boot-mybatis/ManyToManyRelationShip/src/main/java/com/mtb/manytomany/ManyToManyExApplication.java
+++ spring-boot-mybatis/ManyToManyRelationShip/src/main/java/com/mtb/manytomany/ManyToManyExApplication.java
@@ -9,6 +9,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author MithileshB
  * @created 04/07/2020 - 8:16 PM
@@ -30,6 +35,51 @@ public class ManyToManyExApplication implements CommandLineRunner {
         Employee employee = new Employee();
         employee.setFirstName("Mithilesh");
         employee.setLastName("Bhusari");
+
+        /***Initializing hashset using constructors--- START*****/
+        Project[] projectArrayOfMithilesh={new Project("Optus"),
+        new Project("Singtel"),
+        new Project("Telstra"),
+        new Project("TDC NET"),
+        new Project("Virgin Media")};
+
+        Set<Project> projectSetOfMithilesh=new HashSet<>(Arrays.asList(projectArrayOfMithilesh));
+
+        Project[] projectArrayOfMayur={new Project("Optus"),
+        new Project("Singtel"),
+        new Project("NetCracker")};
+        Set<Project> projectSetOfMayur=new HashSet<>(Arrays.asList(projectArrayOfMayur));
+
+
+        Project[] projectArrayOfTarun={new Project("Optus"),
+                new Project("Singtel"),
+                new Project("Barclays")};
+        Set<Project> projectSetOfTarun=new HashSet<>(Arrays.asList(projectArrayOfTarun));
+
+
+        Project[] projectArrayOfAjay={new Project("AT&T"),
+                new Project("Singtel"),
+                new Project("Telstra")};
+        Set<Project> projectSetOfAjay=new HashSet<>(Arrays.asList(projectArrayOfAjay));
+
+        /***Initializing hashset using constructors--- END*****/
+
+        /***Initializing hashset using collections--- START*****/
+
+        Employee[] employeeArray={new Employee("Mithilesh","Bhusari",projectSetOfMithilesh),
+        new Employee("Mayur","Shetye",projectSetOfMayur),
+        new Employee("Tarun","Rohila",projectSetOfTarun),
+        new Employee("Ajay","Sharma",projectSetOfAjay)};
+
+        //Set Demonstration using Collections.addAll
+        Set<Employee> employeeSet= Collections.<Employee>emptySet();
+        Collections.addAll(employeeSet=new HashSet<Employee>(Arrays.asList(employeeArray)));
+
+        //Set Demonstration using Collections.unmodifiable set
+        Set<Employee> employeeSet1=Collections.<Employee>emptySet();
+        Collections.unmodifiableSet(new HashSet<Employee>(Arrays.asList(employeeArray)));
+
+        /***Initializing hashset with collections--- START*****/
 
         // Create project1
         Project project = new Project();
